d489275cb383fe90a085e89575d691de73e62013
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-15 12:31:52 -0500

    period end

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class main {
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for
+        }
+
+
+    }
 }
